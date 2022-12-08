d8856e693c07b5dbdd5f21460e5e29aaffa9d394
DiegoMJMR <103581849+DiegoMJMR@users.noreply.github.com>
2022-12-08 12:37:24 -0300

    Modelo evento

--- api/app/Models/Evento.php
+++ api/app/Models/Evento.php
@@ -22,6 +22,6 @@ class Evento extends Model
     }
 
     public function getEntradasVendidasAttribute() {
-       return $this->entradas->count();
+       return $this->entradas()->count();
     }
 }
